3bfed1487531e727a09086dd2715a3b0630d79dd
Aditya G Kuballi <adityagkuballi@gmail.com>
2024-11-09 17:00:09 +0530

    Final Commit

--- crud.py
+++ crud.py
@@ -29,3 +29,29 @@ def issue_book(book_id, member_id):
         print("Book Issued")
     else:
         print("Book not avilable for issue")
+
+def return_book(transaction_id): 
+    transaction = session.query(Transaction).filter_by(id = transaction_id).first()
+    if transaction and not transaction.return_date:    
+        transaction.return_date = date.today()
+        book = session.query(Book).filter_by(id = transaction.book_id).first()
+        book.count += 1
+        session.commit()
+        print("Book returned to library")
+    else:
+        print("Book already retruned")
+
+def member_Transactions(member_id):
+    transactions = session.query(Transaction).filter_by(member_id = member_id).all()
+    if transactions:
+        return transactions
+    else:
+        print("Member has no transactions")
+
+def delete_book(book_id):
+    book = session.query(Book).filter_by(id = book_id).first()
+    if book and book.count > 0:
+        book.count = 0
+        session.commit()
+    else:
+        print("Book Out of Stock")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from crud import add_book, get_book, add_member, view_member, issue_book
+from crud import add_book, get_book, add_member, view_member, issue_book, return_book, member_Transactions, delete_book
 
 
 def addNewBook():
@@ -36,35 +36,59 @@ def issueABook():
     member_id = int(input("Enter member ID:"))
     issue_book(book_id,member_id)
 
+def returnBook():
+    transaction_id = int(input("Enter transaction ID:"))
+    return_book(transaction_id)
+
+def get_transactions_by_member():
+    member_id = int(input("Enter member ID:"))
+    transactions = member_Transactions(member_id)
+    for transaction in transactions:
+        return_status = "Returned" if transaction.return_date else "Not Returned"
+        print(f"Member transactions Details Book Id: {transaction.book_id}, Book Issue Date: {transaction.issue_date}, Book Return Date: {transaction.return_date}, Return Status: {return_status}")
+
+def deleteBook():
+    book_id = int(input("Enter book Id:"))
+    delete_book(book_id)
+    print("Book Deleted")
+
 def main():
-    print("************************************************")
-    print("1. Add Book")
-    print("2. View Book")
-    print("3. Add Memeber")
-    print("4. View Member")
-    print("5. Issue Book")
-    print("6. Return Book")
-    print("7. View Transactions by Member")
-    print("************************************************")
-    choice=input("Enter your choice: ")
+    while True:
+        print("************************************************")
+        print("1. Add Book")
+        print("2. View Book")
+        print("3. Add Member")
+        print("4. View Member")
+        print("5. Issue Book")
+        print("6. Return Book")
+        print("7. View Transactions by Member")
+        print("8. Delete Book")
+        print("9. Exit")
+        print("************************************************")
+        choice = input("Enter your choice: ")
 
 
-    if choice == "1":
-        addNewBook()
-    elif choice == "2":
-        printBooks()
-    elif choice == "3":
-        addNewMember()
-    elif choice == "4":
-        viewMember()
-    elif choice == "5":
-        issueABook()
-    elif choice == "6":
-        pass
-    elif choice == "7":
-        pass
-    else:
-        print("Invalid choice")
+        if choice == "1":
+            addNewBook()
+        elif choice == "2":
+            printBooks()
+        elif choice == "3":
+            addNewMember()
+        elif choice == "4":
+            viewMember()
+        elif choice == "5":
+            issueABook()
+        elif choice == "6":
+            returnBook()
+        elif choice == "7":
+            get_transactions_by_member()
+        elif choice == "8":
+            deleteBook()
+        elif choice == "9":
+            print("Exited")
+            break
+        else:
+            print("Invalid choice")
 
-if __name__=="__main__":
+if __name__== "__main__":
     main()
